8fe45367328b61dad65300d0be3130c4adcd1fa9
huzorro <huzorro@gmail.com>
2014-01-24 15:49:36 +0800

    repair require error

--- admin/class-wechat-custom-replys-main.php
+++ admin/class-wechat-custom-replys-main.php
@@ -316,7 +316,7 @@ class Wechat_custom_replys_main {
         <div class="wrap">
             <div class="icon32 icon32-posts-post" id="icon-edit"><br></div>
             <h2><?php _e('custom reply', $this->plugin_slug)?>
-                <a class="add-new-h2" href="<?php echo get_admin_url(get_current_blog_id(), 'admin.php?page=replys');?>"><?php _e('back to list', $this->plugin_slug)?></a>
+                <a class="add-new-h2" href="<?php echo get_admin_url(get_current_blog_id(), 'admin.php?page=custom_replys');?>"><?php _e('back to list', $this->plugin_slug)?></a>
             </h2>
 
             <?php if (!empty($notice)): ?>
