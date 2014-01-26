32ab99d7da11b351f8e6117635f83c7eefee1979
huzorro <huzorro@gmail.com>
2014-01-26 16:11:26 +0800

    repair require error

--- admin/includes/class-wechat-custom-replys-list-table.php
+++ admin/includes/class-wechat-custom-replys-list-table.php
@@ -193,8 +193,8 @@ class Wechat_custom_replys_list_table extends WP_List_Table
         // will be used in pagination settings
 //        $total_items = $wpdb->get_var("SELECT COUNT(id) FROM $table_name ");
 
-        $where = isset($_REQUEST['s']) ? " WHERE keyword LIKE '%$_REQUEST[s]%' OR reply_content LIKE '%$_REQUEST[s]%' " : '';
-        $total_items = $wpdb->get_var(sprintf("SELECT COUNT(id) FROM $table_name %s", $where));
+        $condition = isset($_REQUEST['s']) ? " WHERE keyword LIKE '%$_REQUEST[s]%' OR reply_content LIKE '%$_REQUEST[s]%' " : '';
+        $total_items = $wpdb->get_var(sprintf("SELECT COUNT(id) FROM $table_name %s", $condition));
 
         // prepare query params, as usual current page, order by and order direction
         $paged = isset($_REQUEST['paged']) ? max(0, intval($_REQUEST['paged']) - 1) : 0;
@@ -204,8 +204,7 @@ class Wechat_custom_replys_list_table extends WP_List_Table
         // [REQUIRED] define $items array
         // notice that last argument is ARRAY_A, so we will retrieve array
 //        $this->items = $wpdb->get_results($wpdb->prepare("SELECT * FROM $table_name ORDER BY $orderby $order LIMIT %d OFFSET %d", $per_page, $paged * $per_page), ARRAY_A);
-        $this->items = $wpdb->get_results($wpdb->prepare("SELECT * FROM $table_name $where ORDER BY $orderby $order LIMIT %d OFFSET %d", $per_page, $paged * $per_page), ARRAY_A);
-        var_dump($this->items);
+        $this->items = $wpdb->get_results($wpdb->prepare("SELECT * FROM $table_name $condition ORDER BY $orderby $order LIMIT %d OFFSET %d", $per_page, $paged * $per_page), ARRAY_A);
         // [REQUIRED] configure pagination
         $this->set_pagination_args(array(
             'total_items' => $total_items, // total items defined above
