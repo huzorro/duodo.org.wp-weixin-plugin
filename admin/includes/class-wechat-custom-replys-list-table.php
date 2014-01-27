bd27b463b4bd067f87ef87eca749fb8193ec1873
huzorro <huzorro@gmail.com>
2014-01-27 11:49:06 +0800

    repair require error

--- admin/includes/class-wechat-custom-replys-list-table.php
+++ admin/includes/class-wechat-custom-replys-list-table.php
@@ -167,9 +167,34 @@ class Wechat_custom_replys_list_table extends WP_List_Table
             }
         }
     }
-
-    function get_views(){
-        echo "abc";
+    function get_views() {
+        global $wpdb, $post_mime_types, $avail_post_mime_types;
+        $type_links = array();
+        $_num_posts = (array) wp_count_attachments();
+        $_total_posts = array_sum($_num_posts) - $_num_posts['trash'];
+        if ( !isset( $total_orphans ) )
+            $total_orphans = $wpdb->get_var( "SELECT COUNT( * ) FROM $wpdb->posts WHERE post_type = 'attachment' AND post_status != 'trash' AND post_parent < 1" );
+        $matches = wp_match_mime_types(array_keys($post_mime_types), array_keys($_num_posts));
+        foreach ( $matches as $type => $reals )
+            foreach ( $reals as $real )
+                $num_posts[$type] = ( isset( $num_posts[$type] ) ) ? $num_posts[$type] + $_num_posts[$real] : $_num_posts[$real];
+        $class = ( empty($_GET['post_mime_type']) && !$this->detached && !isset($_GET['status']) ) ? ' class="current"' : '';
+        $type_links['all'] = "<a href='upload.php'$class>" . sprintf( _nx( 'All <span class="count">(%s)</span>', 'All <span class="count">(%s)</span>', $_total_posts, 'uploaded files' ), number_format_i18n( $_total_posts ) ) . '</a>';
+        foreach ( $post_mime_types as $mime_type => $label ) {
+            $class = '';
+            if ( !wp_match_mime_types($mime_type, $avail_post_mime_types) )
+                continue;
+            if ( !empty($_GET['post_mime_type']) && wp_match_mime_types($mime_type, $_GET['post_mime_type']) )
+                $class = ' class="current"';
+            if ( !empty( $num_posts[$mime_type] ) )
+                $type_links[$mime_type] = "<a href='upload.php?post_mime_type=$mime_type'$class>" . sprintf( translate_nooped_plural( $label[2], $num_posts[$mime_type] ), number_format_i18n( $num_posts[$mime_type] )) . '</a>';
+        }
+        $type_links['detached'] = '<a href="upload.php?detached=1"' . ( $this->detached ? ' class="current"' : '' ) . '>' . sprintf( _nx( 'Unattached <span class="count">(%s)</span>', 'Unattached <span class="count">(%s)</span>', $total_orphans, 'detached files' ), number_format_i18n( $total_orphans ) ) . '</a>';
+        if ( !empty($_num_posts['trash']) )
+            $type_links['trash'] = '<a href="upload.php?status=trash"' . ( (isset($_GET['status']) && $_GET['status'] == 'trash' ) ? ' class="current"' : '') . '>' . sprintf( _nx( 'Trash <span class="count">(%s)</span>', 'Trash <span class="count">(%s)</span>', $_num_posts['trash'], 'uploaded files' ), number_format_i18n( $_num_posts['trash'] ) ) . '</a>';
+        return $type_links;
+    }
+//    function get_views(){
 //        global $wpdb;
 //        $table_name = $wpdb->prefix . 'wechat_custom_replys';
 //        $type = array_merge(get_option('wechat_msgtype_desc_settings'), get_option('wechat_reply_func_settings') );
@@ -179,7 +204,7 @@ class Wechat_custom_replys_list_table extends WP_List_Table
 //        var_dump($type);
 //        $type_count = $wpdb->get_results($wpdb->prepare($sql), ARRAY_A);
 //        var_dump($type_count);
-
+//
 //        foreach($type as $key => $value) {
 //            $_REQUEST["reply_type"]== $key && $class='class="current"';
 //            foreach($type_count as $k => $v) {
@@ -189,10 +214,9 @@ class Wechat_custom_replys_list_table extends WP_List_Table
 //
 //            unset($class);
 //        }
-        $type_group = array();
-        return $type_group['av'] = "<a href='#'>test</a>";
-
-    }
+//        return $type_group;
+//
+//    }
     /**
      * [REQUIRED] This is the most important method
      *
