f32e80ac282fa6ec639f8275aa3363a6eddb09cd
huzorro <huzorro@gmail.com>
2014-01-29 13:56:38 +0800

    repair require error

--- admin/includes/class-wechat-users-list-table.php
+++ admin/includes/class-wechat-users-list-table.php
@@ -50,7 +50,10 @@ class Wechat_users_list_table extends WP_List_Table
      */
     function column_headimgurl($item)
     {
-        return sprintf("<img src='%s' alt='%s'/>", $item['headimgurl'], $item['nickname']);
+        $actions = array(
+            'view' => sprintf('<a href="?page=users_form&id=%s">%s</a>', $item['id'], __('View history news', $this->plugin_slug))
+        );
+        return sprintf("<img src='%s' alt='%s'/> %s", $item['headimgurl'], $item['nickname'], $this->row_actions($actions));
     }
 
     function column_gender($item)
